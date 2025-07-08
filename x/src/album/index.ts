1aea6108f4f73c54769906f929f31535fa6abff6
axtk <axtkx@inbox.ru>
2025-07-09 00:46:50 +0300

    update album

--- x/src/album/index.ts
+++ x/src/album/index.ts
@@ -4,6 +4,7 @@ render({
     container: 'main',
     navContainer: 'nav',
     publicKey: 'https://disk.yandex.ru/d/m7msMoTw8t0MvQ',
+    indexPublicKey: 'https://disk.yandex.ru/d/cchKlevcXewGXQ',
     path: '0',
     sort: '-name',
     dateFormat: 'short',
