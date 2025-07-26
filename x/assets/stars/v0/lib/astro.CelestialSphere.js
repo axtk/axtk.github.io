56f701e9fc8956cf71f2e482623309bfb577d060
axtk <axtkx@inbox.ru>
2025-07-26 15:40:57 +0300

    restore stars json data to v0

--- x/assets/stars/v0/lib/astro.CelestialSphere.js
+++ x/assets/stars/v0/lib/astro.CelestialSphere.js
@@ -1,6 +1,6 @@
 {
 
-const DATA_LOCATION = 'assets/stars/data';
+const DATA_LOCATION = 'assets/stars/v0/data';
 
 let astro = window.astro = window.astro || {};
 
@@ -16,13 +16,13 @@ const spectralClassColorMap = {
 
 const bayerDesignationMap = {
     'alp': '\u03b1', 'bet': '\u03b2', 'gam': '\u03b3',
-    'del': '\u03b4', 'eps': '\u03b5', 'zet': '\u03b6', 
+    'del': '\u03b4', 'eps': '\u03b5', 'zet': '\u03b6',
     'eta': '\u03b7', 'the': '\u03b8', 'iot': '\u03b9',
-    'kap': '\u03ba', 'lam': '\u03bb', 'mu':  '\u03bc', 
+    'kap': '\u03ba', 'lam': '\u03bb', 'mu':  '\u03bc',
     'nu':  '\u03bd', 'xi':  '\u03be', 'omi': '\u03bf',
-    'pi':  '\u03c0', 'rho': '\u03c1', 'sig': '\u03c3', 
+    'pi':  '\u03c0', 'rho': '\u03c1', 'sig': '\u03c3',
     'tau': '\u03c4', 'ups': '\u03c5', 'phi': '\u03c6',
-    'chi': '\u03c7', 'psi': '\u03c8', 'ome': '\u03c9', 
+    'chi': '\u03c7', 'psi': '\u03c8', 'ome': '\u03c9',
     'omg': '\u03c9'
 };
 
@@ -45,7 +45,7 @@ let util = {
     },
     getStarName(star) {
         let name = '';
-        if (star[7]) name += star[7][1] + 
+        if (star[7]) name += star[7][1] +
             (star[7][2] ? ('<sup>' + star[7][2] + '</sup>') : '') + ' ' + star[7][0];
             // (star[7][2] ? this.toSup(star[7][2]) : '') + ' ' + star[7][0];
         if (star[8]) name = star[8] + ', ' + name;
