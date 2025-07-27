288a7635b90e439f9b01b599fa62c9b055f1cf74
axtk <axtkx@inbox.ru>
2025-07-27 12:55:48 +0300

    update star opacity function

--- x/src/stars/getStarOpacity.ts
+++ x/src/stars/getStarOpacity.ts
@@ -5,5 +5,5 @@ export function getStarOpacity(star: Star, ctx: Context) {
     if (ctx.mode === 'fantasy')
         return Math.min(.5 + .2*(6.75 - star.magnitude), 1);
 
-    return Math.min(.25 + .12*(6.75 - star.magnitude), 1);
+    return Math.min(.2 + .15*(6.75 - star.magnitude), 1);
 }
