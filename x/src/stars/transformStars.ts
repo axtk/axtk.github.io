76bb53dd598757158c30645fa4285f7f5e67f742
axtk <axtkx@inbox.ru>
2025-07-29 08:03:34 +0300

    rearrange columns in star dataset

--- x/src/stars/transformStars.ts
+++ x/src/stars/transformStars.ts
@@ -3,7 +3,7 @@ import {stripQuotes} from './stripQuotes';
 
 export function transformStars(data: string) {
     return data.trim().split(/\r?\n/).slice(1).map(line => {
-        let [ra, dec, magnitude, id, spectralClass, bayerName, properName] = line.split(',');
+        let [ra, dec, magnitude, spectralClass, id, bayerName, properName] = line.split(',');
 
         return new Star({
             ra: Number(ra),
