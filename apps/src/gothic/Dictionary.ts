f753c3dba9f654d54dc881adcda453ec7324c7c7
axtk <axtkx@inbox.ru>
2026-04-15 13:02:00 +0300

    move /apps/gothic to own repo

--- apps/src/gothic/Dictionary.ts
+++
@@ -1,8 +0,0 @@
-export type Dictionary = {
-  /** Term */
-  t: string;
-  /** Definition */
-  d: string;
-  /** Extended definition (with markup) */
-  dx: string;
-}[];
--- apps/src/gothic/SearchResult.ts
+++
@@ -1,9 +0,0 @@
-import type { MatchedItem } from "./MatchedItem.ts";
-
-export type SearchResult = {
-  expressions: {
-    value: string;
-    captureIndex: number;
-  }[];
-  entries: MatchedItem[];
-};
